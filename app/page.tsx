6223ba8f52a6b170405fd90c4a308f4219d89f25
neilsen-ngoi <kenisu222@gmail.com>
2023-11-09 15:32:54 +1300

    nav menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return <div>hi</div>
+  return (
+    <div>
+      <Button>gskdnskfnsdnfkll</Button>
+    </div>
+  )
 }
