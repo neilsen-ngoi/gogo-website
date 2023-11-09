4ad1c1d6befa5846eb606a2ba3fa913661abcd21
neilsen-ngoi <kenisu222@gmail.com>
2023-11-09 17:28:30 +1300

    framermotion

--- app/(landing-page)/firstsection/page.tsx
+++ app/(landing-page)/firstsection/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 const FirstSection = () => {
   return (
@@ -19,10 +22,23 @@ const FirstSection = () => {
             </button>
             <Link href="/contact">
               <button className="bg-gray-500 text-white px-6 py-3 rounded-sm">
-                {' '}
                 Too poor
               </button>
             </Link>
+          </div>
+          <div className="md:pt-10 items-center space-y-10 md:flex justify-center md:mx-auto md:space-x-10">
+            <motion.div
+              initial={{ y: 100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              className="px-10 md:px-0"
+            >
+              <Image
+                src="/images/logo/logo-1.svg"
+                alt="ill"
+                width={400}
+                height={400}
+              />
+            </motion.div>
           </div>
         </div>
       </section>
